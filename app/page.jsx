61626a09deead03b7e370d25d8a2b9aee2601718
Mohammad Anas <anas092121@gmail.com>
2025-10-06 01:55:23 +0530

    Initial commit

---
+++ app/page.jsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <>
+      {/* <Button>wohooooo sever is running</Button> */}
+      <Button variant={"primary"}>primary button</Button>
+    </>
+  );
+}
